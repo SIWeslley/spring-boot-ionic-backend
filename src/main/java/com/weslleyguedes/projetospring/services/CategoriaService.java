997027b4db449584f8654e3284ea02eefae23269
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-17 15:11:41 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/weslleyguedes/projetospring/services/CategoriaService.java
+++ src/main/java/com/weslleyguedes/projetospring/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.weslleyguedes.projetospring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.weslleyguedes.projetospring.domain.Categoria;
 import com.weslleyguedes.projetospring.repositories.CategoriaRepository;
+import com.weslleyguedes.projetospring.services.exceptions.DataIntegrityException;
 import com.weslleyguedes.projetospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
