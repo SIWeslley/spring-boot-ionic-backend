72345059fbd75533aeb52833ef7b93290612ef30
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-21 10:24:14 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/weslleyguedes/projetospring/resources/ClienteResource.java
+++ src/main/java/com/weslleyguedes/projetospring/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.weslleyguedes.projetospring.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.weslleyguedes.projetospring.domain.Categoria;
 import com.weslleyguedes.projetospring.domain.Cliente;
+import com.weslleyguedes.projetospring.dto.CategoriaDTO;
 import com.weslleyguedes.projetospring.dto.ClienteDTO;
+import com.weslleyguedes.projetospring.dto.ClienteNewDTO;
 import com.weslleyguedes.projetospring.services.ClienteService;
 
 @RestController
@@ -31,6 +36,15 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
