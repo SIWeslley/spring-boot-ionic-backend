6aeef02e52b44931df77571f3143eaa15fc1e54c
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-21 13:44:32 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/weslleyguedes/projetospring/dto/ClienteDTO.java
+++ src/main/java/com/weslleyguedes/projetospring/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.weslleyguedes.projetospring.domain.Cliente;
+import com.weslleyguedes.projetospring.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
