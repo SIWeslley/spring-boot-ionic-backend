7e4317db0852d5cbcab2f2d47b1a2411cec56730
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-24 15:14:47 -0300

    Email HTML

--- src/main/java/com/weslleyguedes/projetospring/services/EmailService.java
+++ src/main/java/com/weslleyguedes/projetospring/services/EmailService.java
@@ -1,13 +1,21 @@
 package com.weslleyguedes.projetospring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.weslleyguedes.projetospring.domain.Pedido;
 
 public interface EmailService {
 
+	//Versão texto plano
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void SendEmail(SimpleMailMessage msg);
+	
+	//Versão HTML
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
