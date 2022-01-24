d601cd30f3a22669914d161932fc528003f52e1a
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-24 10:15:04 -0300

    Inserindo Pedido

--- src/main/java/com/weslleyguedes/projetospring/domain/PagamentoComCartao.java
+++ src/main/java/com/weslleyguedes/projetospring/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.weslleyguedes.projetospring.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.weslleyguedes.projetospring.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
