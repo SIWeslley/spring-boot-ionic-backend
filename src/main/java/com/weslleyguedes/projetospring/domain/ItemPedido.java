cb645e22c0553eb09efd425d057ac517766f222a
Weslley Guedes <weslleyguedes.as@gmail.com>
2022-01-24 09:10:52 -0300

    Subototal e total do Pedido

--- src/main/java/com/weslleyguedes/projetospring/domain/ItemPedido.java
+++ src/main/java/com/weslleyguedes/projetospring/domain/ItemPedido.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable{
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@JsonIgnore
@@ -30,6 +30,10 @@ public class ItemPedido implements Serializable{
 		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
+	}
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
 	}
 
 	@JsonIgnore
